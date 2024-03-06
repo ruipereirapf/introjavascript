3a310656c7a5ace8ea63629a831d1372b1827503
Rui Pereira <ruipereira.pf@gmail.com>
2024-03-06 18:21:31 +0000

    kelvin

--- projects/kelvinWeather.js
+++ projects/kelvinWeather.js
@@ -1,13 +1,19 @@
 // Forecast for todays weather in Kelvin
-const kelvin = 293
+const kelvin = 0;
 
 // Converting Kelvin to Celsius by subtracting 273
-const celsius = kelvin - 273
+const celsius = kelvin - 273;
 
 // Converting Celsius to Fahrenheit
-let fahrenheit = celsius * (9/5) + 32
+let fahrenheit = celsius * (9/5) + 32;
 
 // Rounds up Fahrenheit value
-fahrenheit = Math.floor(fahrenheit)
+fahrenheit = Math.floor(fahrenheit);
 
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`)
+
+// Convert Celsius into Newtow
+let newton = celsius * (33/100);
+
+// Rounds up the value
+console.log(Math.floor(newton));
