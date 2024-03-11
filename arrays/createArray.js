26c6d2fcb8589e2d97d274a68a060c2dd02b0232
Rui Pereira <ruipereira.pf@gmail.com>
2024-03-11 20:48:54 +0000

    arrays

---
+++ arrays/createArray.js
@@ -0,0 +1,3 @@
+const hobbies = ['hoobie1','hoobie2','hoobie3'];
+
+console.log(hobbies);
---
+++ arrays/popArray.js
@@ -0,0 +1,7 @@
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+console.log(chores);
+
+const removed = chores.pop();
+console.log(removed);
+
+console.log(chores);
