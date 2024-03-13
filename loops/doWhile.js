f89de201fdc58e2661365fe8331d0dcc0044ad76
Rui Pereira <ruipereira.pf@gmail.com>
2024-03-13 14:15:58 +0000

    loops

---
+++ loops/doWhile.js
@@ -0,0 +1,6 @@
+const cupsOfSugarNeeded = 4;
+let cupsAdded = 0;
+
+do{
+ cupsAdded ++;
+} while (cupsAdded < cupsOfSugarNeeded);
---
+++ loops/manualRepeat.js
@@ -0,0 +1,7 @@
+// Write your code below
+
+const vacationSpots = ['Place A','Place B','Place C'];
+
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
