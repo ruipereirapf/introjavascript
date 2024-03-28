b77406af5ab1de5a17a1e2252fcdbfad8b64c9e0
Rui Pereira <ruipereira.pf@gmail.com>
2024-03-28 15:44:31 +0000

    Secret message

--- projects/trainingDays.js
+++ projects/trainingDays.js
@@ -3,53 +3,52 @@
 
 const getRandEvent = () => {
     const random = Math.floor(Math.random() * 3);
-    
+
     if (random === 0) {
-      return 'Marathon';
+        return 'Marathon';
     } else if (random === 1) {
-      return 'Triathlon';
+        return 'Triathlon';
     } else if (random === 2) {
-      return 'Pentathlon';
+        return 'Pentathlon';
     }
-  };
+};
   
-  // The scope of `days` is too tight 
-  const getTrainingDays = event => {
+// The scope of `days` is too tight 
+const getTrainingDays = event => {
     let days = 0;
     if (event === 'Marathon') {
-      days = 50;
+        days = 50;
     } else if (event === 'Triathlon') {
-      days = 100;
+        days = 100;
     } else if (event === 'Pentathlon') {
-      days = 200;
+        days = 200;
     }
-  
     return days;
-  };
+};
   
-  const name = 'Nala';
-  
-  // The scope of `name` is too tight 
-  const logEvent = (name, event) => {
+const name = 'Nala';
+
+// The scope of `name` is too tight 
+const logEvent = (name, event) => {
     console.log(`${name}'s event is: ${event}`);
-  };
-  
-  const logTime = (name, days) => {
+};
+
+const logTime = (name, days) => {
     console.log(`${name}'s time to train is: ${days} days`);
-  };
+};
+
+const event = getRandEvent();
+const days = getTrainingDays(event);
+// Define a `name` variable. Use it as an argument after updating logEvent and logTime 
   
-  const event = getRandEvent();
-  const days = getTrainingDays(event);
-  // Define a `name` variable. Use it as an argument after updating logEvent and logTime 
   
-  
-  logEvent(name, event);
-  logTime(name, days);
-  
-  const event2 = getRandEvent();
-  const days2 = getTrainingDays(event2);
-  const name2 = 'Warren';
-  
-  logEvent(name2, event2);
-  logTime(name2, days2);
+logEvent(name, event);
+logTime(name, days);
+
+const event2 = getRandEvent();
+const days2 = getTrainingDays(event2);
+const name2 = 'Warren';
+
+logEvent(name2, event2);
+logTime(name2, days2);
   
